ebbc132ee7f8e905503c1e1cdf8c0bced37d63f1
Mathew-R <Mathew.r@bronxsoftware.org>
2016-06-04 20:59:31 -0400

    Create Games.js

---
+++ Games.js
@@ -0,0 +1,22 @@
+//		javascript code
+//		script_name: Games
+//
+//		author: Mathew R and Miguel Mejia
+//		description: We both like games 
+//
+
+init(); 
+setTempo(120); 
+base1 = DUBSTEP_DRUMLOOP_MAIN_008
+drums1 = Y01_DRUMS_1
+drums2 = Y02_DRUM_SAMPLES_1
+base2 = DUBSTEP_DRUMLOOP_MAIN_009
+RnB = RD_RNB_808MAINBEAT_2
+RnB2 = RD_RNB_808MAINBEAT_6
+fitMedia(base1, 1, 1, 5);
+fitMedia(drums1, 1, 5, 7);
+fitMedia(drums2, 2, 1, 5);
+fitMedia(base2, 2, 5, 7);
+fitMedia(RnB, 1, 7, 10);
+fitMedia(RnB2, 2, 7, 10);
+finish(); 
